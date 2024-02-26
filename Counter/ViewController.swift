bcdf57c044bca8676264314c89674e192cf5270b
Magomed <mg.abd@yandex.ru>
2024-02-26 15:19:48 +0300

    Сложная задача загружена

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -5,20 +5,55 @@
 //  Created by Mag Abd on 25.02.2024.
 //
 
-import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var historyField: UITextView!
+    @IBOutlet weak var makeCntZero: UIButton!
+    @IBOutlet weak var bottonCounterMinus: UIButton!
     @IBOutlet weak var bottonCounterPlus: UIButton!
     @IBOutlet weak var textLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    var cnt = 0
+    var count = 0
+    var todayDate = Date()
     @IBAction func bottonDidTap(_ sender: Any) {
-        cnt += 1
-        textLabel.text = "Значение счётчика: \(cnt)"
+        count += 1
+        textLabel.text = "Значение счётчика: \(count)"
+        var dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        var dateString = dateFormatter.string(from: todayDate)
+        historyField.text = "\(dateString) - Значение изменено на +1"
+    }
+    @IBAction func bottondDidTapMinus(_ sender: Any) {
+        if count > 0 {
+            count -= 1
+            var dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            dateFormatter.locale = Locale(identifier: "ru_Ru")
+            var dateString = dateFormatter.string(from: todayDate)
+            historyField.text = "\(dateString) - Значение изменено на -1"
+        } else {
+            count = 0
+            var dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            dateFormatter.locale = Locale(identifier: "ru_Ru")
+            var dateString = dateFormatter.string(from: todayDate)
+            historyField.text = "\(dateString) - Попытка уменьшить значение счётчика ниже 0"
+        }
+        textLabel.text = "Значение счётчика: \(count)"
+    }
+    @IBAction func makeCntZeroAction(_ sender: Any) {
+        count = 0
+        textLabel.text = "Значение счётчика: \(count)"
+        var dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        var dateString = dateFormatter.string(from: todayDate)
+        historyField.text = "\(dateString) - Значение изменено на 0"
     }
 }
 
